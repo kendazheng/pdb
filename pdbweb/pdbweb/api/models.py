d844b8a3c83d4233aeb93ece1cabd242e7e76fd4
kendazheng <kendazheng@163.com>
2014-03-21 00:22:08 +0800

    start to design article table

--- pdbweb/pdbweb/api/models.py
+++ pdbweb/pdbweb/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User)
+    tag = models.ForeignKey(Tag)
+    title = models.CharField(max_length=200) 
+    content = models.TextField()
+    publish_date = models.DateTimeField(auto_now=True)
+    publish_state = models.BooleanField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article)
+    father = models.ForeignKey('self') 
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    commenter = models.ForeignKey(User)
