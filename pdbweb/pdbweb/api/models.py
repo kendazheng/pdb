a4af3dfcc0f2c4e33a7bf961ce14b65c0009727e
kendazheng <kendazheng@163.com>
2015-04-01 01:21:23 +0800

    修改pep8格式

--- pdbweb/pdbweb/api/models.py
+++ pdbweb/pdbweb/api/models.py
@@ -1,28 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Article(models.Model):
     TAG_CHOICE = (
-        ('ENTERTAINMENT','entertainment'),
-        ('SPORT','sport'),
-        ('CULTURE','culture'),
-        ('TRAVEL','travel'),
-        ('TECHNOLOGY','technology'),
-        ('FOOD','food'),
-        ('FASHION','fashion'),
-        ('OTHERS','others')
+        ('ENTERTAINMENT', 'entertainment'),
+        ('SPORT', 'sport'),
+        ('CULTURE', 'culture'),
+        ('TRAVEL', 'travel'),
+        ('TECHNOLOGY', 'technology'),
+        ('FOOD', 'food'),
+        ('FASHION', 'fashion'),
+        ('OTHERS', 'others')
     )
-    author = models.ForeignKey(User,related_name='article')
+    author = models.ForeignKey(User, related_name='article')
     title = models.CharField(max_length=200)
-    tag = models.CharField(max_length=20,choices=TAG_CHOICE,default='ENTERTAINMENT')
+    tag = models.CharField(max_length=20, choices=TAG_CHOICE, default='ENTERTAINMENT')
     index = models.URLField(max_length=500)
     summary = models.TextField(max_length=1000)
     publish_date = models.DateTimeField(auto_now=True)
     publish_state = models.BooleanField()
 
     def __unicode__(self):
-        return '%s:%s' %(self.author, self.title)
+        return '%s:%s' % (self.author, self.title)
+
 
 class Content(models.Model):
     article = models.ForeignKey(Article, related_name='contents')
@@ -33,40 +35,57 @@ class Content(models.Model):
         unique_together = ('article', 'title')
 
     def __unicode__(self):
-        return '%s:%s' %(self.article, self.title)
+        return '%s:%s' % (self.article, self.title)
+
 
 class Media(models.Model):
     MEDIA_CHOICE = (
-        ('IMAGE','image'),
-        ('VIDEO','video')
+        ('IMAGE', 'image'),
+        ('VIDEO', 'video')
     )
     content = models.ForeignKey(Content, related_name='medias')
     src = models.URLField(max_length=500)
-    media_type = models.CharField(max_length=10,choices=MEDIA_CHOICE,default='IMAGE') 
+    media_type = models.CharField(max_length=10, choices=MEDIA_CHOICE, default='IMAGE')
    
     class Meta:
         unique_together = ('content', 'src')
      
     def __unicode__(self):
-        return '%s:%s' %(self.content,self.media_type)
+        return '%s:%s' % (self.content, self.media_type)
+
 
 class Comment(models.Model):
     article = models.ForeignKey(Article)
-    father = models.ForeignKey('self') 
+    father = models.ForeignKey('self')
     content = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
     commenter = models.ForeignKey(User)
 
-class Draft(models.Model):
-    author = models.ForeignKey(User)
-    article = models.ForeignKey(Article)
 
-    def __unicode__(self):
-        return '%s:%s' %(self.author,self.article)
+class Draft(models.Model):
+    TAG_CHOICE = (
+        ('ENTERTAINMENT', 'entertainment'),
+        ('SPORT', 'sport'),
+        ('CULTURE', 'culture'),
+        ('TRAVEL', 'travel'),
+        ('TECHNOLOGY', 'technology'),
+        ('FOOD', 'food'),
+        ('FASHION', 'fashion'),
+        ('OTHERS', 'others')
+    )
+    author = models.ForeignKey(User, related_name='article')
+    title = models.CharField(max_length=200)
+    tag = models.CharField(max_length=20, choices=TAG_CHOICE, default='ENTERTAINMENT')
+    index = models.URLField(max_length=500)
+    summary = models.TextField(max_length=1000)
+    publish_date = models.DateTimeField(auto_now=True)
+    publish_state = models.BooleanField()
+    content = models.TextField(max_length=2000)
+
 
 class Album(models.Model):
     album_name = models.CharField(max_length=100)
-    #artist = models.CharField(max_length=100)
+    # artist = models.CharField(max_length=100)
     artist = models.ForeignKey(User, related_name='artist')
     
 
--- pdbweb/pdbweb/api/serializers.py
+++ pdbweb/pdbweb/api/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from pdbweb.api.models import *
 
+
 class LoginObject(object):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=30)
@@ -13,44 +15,59 @@ class LoginSerializer(serializers.Serializer):
 
     def restore_object(self, attrs, instance=None):
         if instance is not None:
-            instance.username = attrs.get('username',instance.username)
-            instance.password = attrs.get('password',instance.password)
+            instance.username = attrs.get('username', instance.username)
+            instance.password = attrs.get('password', instance.password)
             return instance
 
-        return LoginObject(**attrs)    
+        return LoginObject(**attrs)
+
 
 class RegistrySerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password','email') 
+        fields = ('username', 'password', 'email')
+
 
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Media
         fields = ('src', 'media_type')
 
+
 class ContentSerializer(serializers.ModelSerializer):
-    medias = MediaSerializer(many=True)    
+    medias = MediaSerializer(many=True)
+
     class Meta:
         model = Content
         fields = ('title', 'desc', 'medias')
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     contents = ContentSerializer(many=True)
     author = serializers.SlugRelatedField(slug_field='username')
+    
     class Meta:
         model = Article
-        #fields = ('id', 'title', 'author', 'index', 'summary', 'publish_date', 'publish_state', 'contents','tag')
+        # fields = ('id', 'title', 'author', 'index', 'summary', 'publish_date', 'publish_state', 'contents','tag')
         read_only_fields = ('id',)
+
 
 class TrackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Track
         fields = ('order', 'title', 'duration')
 
+
 class AlbumSerializer(serializers.ModelSerializer):
     tracks = TrackSerializer(many=True)
     artist = serializers.SlugRelatedField(slug_field='username')
+
     class Meta:
         model = Album
         fields = ('album_name', 'artist', 'tracks')
+
+
+class DraftSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Draft
+        fields = ()
